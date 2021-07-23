3e0531f6f443dc257a9508e9c713d79b49a60dd7
TristenMarto <tristen.marto@gmail.com>
2021-07-23 10:31:51 +0200

    New class for data specific tasks

---
+++ handle_data.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from imblearn.datasets import fetch_datasets
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.decomposition import PCA
+
+class HandleData :
+    
+    def load_data(self, name) :
+        
+        datasets = fetch_datasets()
+        data = datasets[name]
+        X,y,title = data['data'], data['target'], data['DESCR']
+        le = LabelEncoder()
+        y_enc = le.fit_transform(y)
+
+        return X, y_enc, title
+
+    def pca_plot(self, X, y) :
+        
+        counter = Counter(y)
+        x = StandardScaler().fit_transform(X)
+        pca = PCA(n_components=2)
+        principalComponents = pca.fit_transform(x)
+        principalDf = pd.DataFrame(data = principalComponents
+                    , columns = ['principal component 1', 'principal component 2'])
+        
+        fig = plt.figure(figsize = (12,7))
+        ax = fig.add_subplot(1,1,1) 
+        ax.set_xlabel('Principal Component 1')
+        ax.set_ylabel('Principal Component 2')
+        ax.set_title(f'2 component PCA')
+
+        for label, _ in counter.items() :
+            rowix = np.where(y == label)[0]
+            ax.scatter(principalComponents[rowix, 0], principalComponents[rowix, 1], label=str(label))
+
+        ax.legend()
+        fig.show()
+        print(counter)
+
+    def divide_num_cat(self, X, threshold) :
+
+        cat_columns = []
+        num_columns = []
+
+        for i in range(X.shape[1]) :
+            if len(np.unique(X[:,i])) < threshold :
+                cat_columns.append(i)
+            else :
+                num_columns.append(i)
+
+        return cat_columns, num_columns
+
+    def find_min_maj(self, y) :
+
+        v,c = np.unique(y, return_counts = True)
+        majority = c[0]
+        minority = c[1]
+        min_prop = minority/majority
+
+        return majority, minority, min_prop
+
+    def analyse_dataset(self, dataset, proportions) :
+        
+        X,y,title = self.load_data(dataset)
+        cat_columns, num_columns = self.divide_num_cat(X, 20)
+        majority, minority, min_prop = self.find_min_maj(y)
+        possible_props = proportions[proportions > min_prop] 
+
+        f_dict = {
+            'title': title,
+            'samples': X.shape[0],
+            'features': X.shape[1],
+            'cat_columns': cat_columns,
+            'cat_features': len(cat_columns),
+            'num_columns': num_columns,
+            'num_features': len(num_columns),
+            'minority': minority,
+            'possible_proportions': possible_props
+        }
+
+        return f_dict
